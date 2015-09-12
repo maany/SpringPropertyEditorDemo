37dc960057a6dd62909c609713d4216faf64e7d2
maany <imptodefeat@gmail.com>
2015-09-12 23:32:57 +0530

    DAO added to spring bean management

--- src/com/maany/spring/dao/ClientDAO.java
+++ src/com/maany/spring/dao/ClientDAO.java
@@ -1,28 +1,16 @@
 package com.maany.spring.dao;
 
 import com.maany.spring.model.Client;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 /**
  * Created by Mayank on 9/12/2015.
  */
+@Repository
 public class ClientDAO {
     private List<Client> clients;
-    private static ClientDAO instance;
-
-    private ClientDAO() {
-    }
-
-    /**
-     * Client follows Singleton Pattern. Use this method to get the instance.
-     * @return
-     */
-    public static ClientDAO getInstance(){
-        if(instance==null)
-            instance = new ClientDAO();
-        return instance;
-    }
 
     /**
      * add new clients
---
+++ src/com/maany/spring/service/ClientRegistrationService.java
@@ -0,0 +1,11 @@
+package com.maany.spring.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Mayank on 9/12/2015.
+ */
+@Service
+public class ClientRegistrationService {
+
+}

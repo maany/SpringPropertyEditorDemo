0fbbcd1d773005c713a2fb8dd93d1e3bafd8ebb7
maany <imptodefeat@gmail.com>
2015-09-24 19:11:04 +0530

    update Client feature tested

--- src/com/maany/spring/controller/ViewEditClientController.java
+++ src/com/maany/spring/controller/ViewEditClientController.java
@@ -1,13 +1,16 @@
 package com.maany.spring.controller;
 
+import com.maany.spring.model.Address;
 import com.maany.spring.model.Client;
+import com.maany.spring.model.GrantType;
+import com.maany.spring.propertyeditor.CollectionPropertyEditor;
 import com.maany.spring.service.ClientRegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -25,4 +28,21 @@ public class ViewEditClientController {
         ModelAndView modelAndView = new ModelAndView("viewEditClient","model",map);
         return modelAndView;
     }
+    @RequestMapping(value="{id}", method = RequestMethod.POST)
+    public ModelAndView updateClient(@PathVariable("id") Integer id,ModelMap map,@ModelAttribute("client") Client client,BindingResult errors){
+        ModelAndView modelAndView = new ModelAndView("viewEditClient","model",map);
+        return modelAndView;
+    }
+
+    @InitBinder
+    public void setBinders(WebDataBinder binder){
+        CollectionPropertyEditor collectionPropertyEditor = new CollectionPropertyEditor(Address.class);
+        CollectionPropertyEditor grantTypeCollectionPropertyEditor = new CollectionPropertyEditor(GrantType.class);
+        binder.registerCustomEditor(getNewClient().getAuthorizedGrantTypes().getClass(),grantTypeCollectionPropertyEditor);
+        binder.registerCustomEditor(getNewClient().getAddressCollection().getClass(),collectionPropertyEditor);
+
+    }
+
+    public Client getNewClient(){ return new Client();};
+
 }
